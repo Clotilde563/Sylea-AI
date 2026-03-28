bd36303b4a2ed1511febbfb68df0f94ec5ddf4ed
Clotilde563 <Deniauclotilde55@gmail.com>
2026-03-28 16:45:01 +0100

    fix: CORS allow all origins temporarily for production debugging

--- api/main.py
+++ api/main.py
@@ -64,7 +64,7 @@ if extra_origins:
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
